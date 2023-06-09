b373608fef63b160d640dd2d7f8d1bb3ae0a0a0c
Ian Dave Lanaja <idelanaja@mail.com>
2023-06-09 13:35:23 +0800

    chages

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
 }
 
-
-// Importing Libraies that we installed using npm
 const express = require("express")
 const app = express()
-const bcrypt = require("bcrypt") // Importing bcrypt package
+const bcrypt = require("bcrypt")
 const passport = require("passport")
 const initializePassport = require("./passport-config")
 const flash = require("express-flash")
 const session = require("express-session")
 const methodOverride = require("method-override")
+const bodyParser = require('body-parser')
+
 
 initializePassport(
     passport,
@@ -23,11 +23,30 @@ initializePassport(
 
 const users = []
 
+const currentUser = []
+
+const events = [
+    {
+      id: 1,
+      title: 'Event 1',
+      description: 'Description of Event 1'
+    },
+    {
+      id: 2,
+      title: 'Event 2',
+      description: 'Description of Event 2'
+    }
+  ];
+
+
+const registrations = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({extended: false}))
 app.use(flash())
 app.use(session({
     secret: process.env.SESSION_SECRET,
-    resave: false, // We wont resave the session variable if nothing is changed
+    resave: false, 
     saveUninitialized: false
 }))
 app.use(passport.initialize()) 
@@ -53,6 +72,11 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
             password: hashedPassword,
         })
         console.log(users); // Display newly registered in the console
+        currentUser.push( {
+            name: req.body.name,
+            email: req.body.email,
+        })
+        console.log(currentUser)
         res.redirect("/login")
         
     } catch (e) {
@@ -63,7 +87,7 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
 
 // Routes
 app.get('/', checkAuthenticated, (req, res) => {
-    res.render("index.ejs", {name: req.user.name})
+    res.render("index.ejs", {name: req.user.name, events,registrations})
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
@@ -89,6 +113,7 @@ app.delete("/logout", (req, res) => {
 
 function checkAuthenticated(req, res, next){
     if(req.isAuthenticated()){
+        
         return next()
     }
     res.redirect("/login")
@@ -100,5 +125,26 @@ function checkNotAuthenticated(req, res, next){
     }
     next()
 }
+
+// Para handle sa registration form submission
+app.post('/submit-registration', (req, res) => {
+    const { eventId } = req.body
+    const event = events.find(event => event.id === parseInt(eventId))
+    var name = currentUser.name
+    var email = currentUser.email
+    if (event) {
+      const registration = {
+        name: name,
+        email: email,
+        eventTitle: event.title
+      };
+      registrations.push(registration)
+      res.redirect('/')
+      console.log(registration)
+    } else {
+      res.status(404).send('Event not found');
+    }
+  });
+
 
 app.listen(3000)
